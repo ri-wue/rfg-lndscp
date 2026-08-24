eb3bd1554418fd84773292fe54a116f1dbb20b33
ri-wue <enriquerefugio@icloud.com>
2026-08-24 16:11:46 -0700

    Started the project off Xcode app template

---
+++ Refugio Lndscpng/Refugio Lndscpng/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Refugio Lndscpng
+//
+//  Created by en.riwue on 8/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Refugio Lndscpng/Refugio Lndscpng/Refugio_LndscpngApp.swift
@@ -0,0 +1,17 @@
+//
+//  Refugio_LndscpngApp.swift
+//  Refugio Lndscpng
+//
+//  Created by en.riwue on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Refugio_LndscpngApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
