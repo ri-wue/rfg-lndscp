d126c9b0fb879ac05617e6a4503f3eb472656c34
ri-wue <enriquerefugio@icloud.com>
2026-08-26 17:45:36 -0700

    Still figuring things out

--- Refugio Lndscpng/Refugio Lndscpng/ContentView.swift
+++ Refugio Lndscpng/Refugio Lndscpng/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("Refugio Landscaping")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.verde)
+                    .padding()
+                Spacer()
+            }
+            HStack{
+                Button("Aprieta el Botón!") {
+                    print("Boo! 👻")
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+                .buttonBorderShape(.capsule)
+                .tint(.tuna)
+            }
         }
-        .padding()
     }
 }
 
